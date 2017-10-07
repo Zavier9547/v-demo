f5c2498292198a866b7d0ab99a1a2534494c919e
zhaiwei <zhaiwei@qq.com>
2017-10-07 23:20:21 +0800

    小改了些样式，将验证码功能demo抽象化

--- app/src/main/java/com/example/vdemo/VerificationActivity.java
+++ app/src/main/java/com/example/vdemo/VerificationActivity.java
@@ -2,12 +2,42 @@ package com.example.vdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class VerificationActivity extends Activity {
+public class VerificationActivity extends Activity implements View.OnClickListener {
+
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+
+        init();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Toast.makeText(this,"验证码发送成功：1234",Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this,"验证成功",Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+        }
+    }
+
+    private void init() {
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
     }
 }
